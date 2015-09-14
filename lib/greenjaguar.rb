11dbd7fa7a3da1a5a488b33263eb6a2b34760058
Aniruddha Deshpande <aniruddha.deshpande@salesforce.com>
2015-09-14 10:43:30 -0700

    bug fix

--- lib/greenjaguar.rb
+++ lib/greenjaguar.rb
@@ -8,18 +8,11 @@ require 'greenjaguar/policy_builder'
 require 'greenjaguar/retrier'
 
 module Greenjaguar
-
-  def self.included(klass)
-    klass.extend(ClassMethods)
+  def build_policy(&block)
+    PolicyBuilder.new(&block)
   end
 
-  module ClassMethods
-    def build_policy(&block)
-      PolicyBuilder.new(&block)
-    end
-
-    def robust_retry(policy, &block)
-      Retrier.new(policy, &block)
-    end
+  def robust_retry(policy, &block)
+    Retrier.new(policy, &block)
   end
 end
--- spec/greenjaguar_spec.rb
+++ spec/greenjaguar_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
 module Specs
-  include Greenjaguar
-
   describe Greenjaguar do
-    let(:class_under_test) { Class.new { include Greenjaguar } }
+    class ObjectUnderTest
+      include Greenjaguar
+    end
 
     before do
+      @object_under_test = ObjectUnderTest.new
       @stub = stub_request(:get, "http://www.example.com").to_raise("some error")
     end
 
@@ -15,12 +16,12 @@ module Specs
     end
 
     it '#run should call the passed code block 4 times' do
-      policy = class_under_test.build_policy do
+      policy = @object_under_test.build_policy do
         times 3
       end
 
       expect do
-        class_under_test.robust_retry(policy) do
+        @object_under_test.robust_retry(policy) do
           Net::HTTP.get_response(URI.parse("http://www.example.com"))
         end
       end.to raise_error
@@ -30,37 +31,37 @@ module Specs
     it '#run should call the passed code block only 1 time if successful response is received' do
       @stub = stub_request(:get, "http://www.example.com")
 
-      policy = class_under_test.build_policy do
+      policy = @object_under_test.build_policy do
         times 3
       end
 
-      class_under_test.robust_retry(policy) do
+      @object_under_test.robust_retry(policy) do
         Net::HTTP.get_response(URI.parse("http://www.example.com"))
       end
       assert_requested :get, "http://www.example.com", :times => 1
     end
 
     it '#run should raise the error once retrying is completed' do
-      policy = class_under_test.build_policy do
+      policy = @object_under_test.build_policy do
         times 3
       end
 
       expect do
-        class_under_test.robust_retry(policy) do
+        @object_under_test.robust_retry(policy) do
           Net::HTTP.get_response(URI.parse("http://www.example.com"))
         end
       end.to raise_error
     end
 
     it '#run should call the passed code block 4 times according to fibonacci sequence' do
-      policy = class_under_test.build_policy do
+      policy = @object_under_test.build_policy do
         times 3
         with_strategy :fibonacci
         measure_time_in :ms
       end
 
       expect do
-        class_under_test.robust_retry(policy) do
+        @object_under_test.robust_retry(policy) do
           Net::HTTP.get_response(URI.parse("http://www.example.com"))
         end
       end.to raise_error
@@ -68,14 +69,14 @@ module Specs
     end
 
     it '#run should call the passed code block 4 times according to fixed interval strategy' do
-      policy = class_under_test.build_policy do
+      policy = @object_under_test.build_policy do
         times 3
         with_strategy :fixed_interval, 2
         measure_time_in :ms
       end
 
       expect do
-        class_under_test.robust_retry(policy) do
+        @object_under_test.robust_retry(policy) do
           Net::HTTP.get_response(URI.parse("http://www.example.com"))
         end
       end.to raise_error
@@ -83,14 +84,14 @@ module Specs
     end
 
     it '#run should call the passed code block 4 times according to exponential backoff sequence' do
-      policy = class_under_test.build_policy do
+      policy = @object_under_test.build_policy do
         times 5
         with_strategy :exponential_backoff
         measure_time_in :ms
       end
 
       expect do
-        class_under_test.robust_retry(policy) do
+        @object_under_test.robust_retry(policy) do
           Net::HTTP.get_response(URI.parse("http://www.example.com"))
         end
       end.to raise_error
@@ -99,14 +100,14 @@ module Specs
 
     it '#run does not call the passed code block if exception is not part of allowed exception(s)' do
       @stub = stub_request(:get, "www.example.com").to_raise(RegexpError)
-      policy = class_under_test.build_policy do
+      policy = @object_under_test.build_policy do
         times 5
         with_strategy :fibonacci
         only_on_exceptions [ZeroDivisionError]
       end
 
       expect do
-        class_under_test.robust_retry(policy) do
+        @object_under_test.robust_retry(policy) do
           Net::HTTP.get_response(URI.parse("http://www.example.com"))
         end
       end.to raise_error
@@ -116,7 +117,7 @@ module Specs
     it '#run should call the passed code block if exception is part of allowed exception(s)' do
       @stub = stub_request(:get, "http://www.example.com").to_raise(ZeroDivisionError)
 
-      policy = class_under_test.build_policy do
+      policy = @object_under_test.build_policy do
         times 10
         with_strategy :fibonacci
         measure_time_in :ms
@@ -124,7 +125,7 @@ module Specs
       end
 
       expect do
-        class_under_test.robust_retry(policy) do
+        @object_under_test.robust_retry(policy) do
           Net::HTTP.get_response(URI.parse("http://www.example.com"))
         end
       end.to raise_error
@@ -132,12 +133,12 @@ module Specs
     end
 
     it '#run should not raise the error if set to fail silently' do
-      policy = class_under_test.build_policy do
+      policy = @object_under_test.build_policy do
         times 3
         fail_silently
       end
 
-      class_under_test.robust_retry(policy) do
+      @object_under_test.robust_retry(policy) do
         Net::HTTP.get_response(URI.parse("http://www.example.com"))
       end
     end
